24ff57f4b24c51a3b063f5e0cef6e5c762a3d1c9
Nick Porcino <meshula@hotmail.com>
2018-10-13 22:58:07 -0700

    Fix offscreen blit

--- assets/pipelines/deferred/full-screen-deferred-blit-fsh.glsl
+++ assets/pipelines/deferred/full-screen-deferred-blit-fsh.glsl
@@ -4,13 +4,11 @@ void main()
     vec3 normal = texture(u_normalTexture, vert.v_texCoord).xyz;
     vec3 light = normalize(vec3(0.1, 0.4, 0.2));
     float i = dot(normal, normal);
-    if (i >= 0.0) {
-        o_colorTexture = texture(u_diffuseTexture, vert.v_texCoord);
-    }
-    else {
+    if (i > 0.0) {
         i = dot(normal, light);
         o_colorTexture = texture(u_diffuseTexture, vert.v_texCoord) * vec4(i,i,i, 1);
     }
-    i = dot(normal, normal);
-    o_colorTexture = vec4(i,i,i, 1);
+    else {
+        o_colorTexture = texture(u_diffuseTexture, vert.v_texCoord);
+    }
 }
