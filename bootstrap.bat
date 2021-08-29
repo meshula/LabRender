d0af26d4c06db5a374aa37698857fb5775538d5f
Nick Porcino <meshula@hotmail.com>
2021-08-28 22:38:51 -0700

    Refer to USD and Effekseer from an install dir as they are both too heavy for a submodule

---
+++ bootstrap.bat
@@ -0,0 +1,16 @@
+
+echo installing Effekseer buildroot: %1 install:%2
+git clone --recursive https://github.com/effekseer/Effekseer %1/deps/Effekseer
+pushd %1/deps/Effekseer
+mkdir build
+cd build
+cmake .. -DUSE_MSVC_RUNTIME_LIBRARY_DLL=ON -DCMAKE_INSTALL_PREFIX=%2 -DBUILD_GL=ON -DBUILD_EXAMPLES=OFF -DBUILD_EDITOR=OFF -DBUILD_DX9=OFF -DBUILD_DX11=OFF -DBUILD_DX12=OFF -DBUILD_METAL=OFF -DUSE_OPENGL3=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo
+cmake --build . --target install --config RelWithDebInfo
+popd
+
+echo installing USD buildroot: %1 install:%2
+git clone https://github.com/PixarAnimationStudios/USD.git %1/deps/USD
+python %1/deps/USD/build_scripts/build_usd.py --no-python --no-tools --no-usdview --draco --materialx %2
+
+echo add the following to the cmake configuration for LabRender:
+echo    -DVFX_ROOT=%2
